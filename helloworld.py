2b619b9f635e77b328f72be8b71ef3cbc7e0c132
Aleksandra Balashova <ealba003@edu.xamk.fi>
2023-09-13 11:50:43 +0300

    notes.py application file added

--- helloworld.py
+++ helloworld.py
@@ -8,3 +8,7 @@ print(userInput)
 
 x = 5
 print(type(x))
+
+for i in range(1, 20):
+    if i % 2 == 0:
+        print(i)
---
+++ notes.py
@@ -0,0 +1,12 @@
+# notes.py - simple notes application
+
+# modules
+import sys
+
+# main function
+def main() -> int:
+    return(0)
+
+# main function definition
+if __name__ == '__main__':
+    sys.exit(main())
