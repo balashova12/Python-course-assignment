a366002d04050f283dfb10cd8aa61e02a5a157cf
Aleksandra Balashova <ealba003@edu.xamk.fi>
2023-09-13 12:31:58 +0300

    notes.py print commands deleted

--- notes.py
+++ notes.py
@@ -5,18 +5,18 @@ import sys
 
 # main function
 def main() -> int:
-    # Arguments by sys.argv 
-    print(sys.argv)
-    print(sys.argv[0])
-    print(sys.argv[1])
-    print(sys.argv[2])
-    print(sys.argv[1] + sys.argv[2])
-    print(23 + 59)
-    a = int(sys.argv[1])
-    b = int(sys.argv[2])
-    print(a + b)
+    # login screen
+    print("")
+    username = input("Username: ")
+    # password request needs to be added here
+    password = input("Password: ")
+    print("")
 
-    # Return value
+    # Notes  screen 
+    print("1. some text")
+    print("2. ...")
+    input("Selection: ")
+
     return(0)
 
 # main function definition
