b5fc6f5f6c3fc1ea65596a28a941d938cc78c0a3
Aleksandra Balashova <ealba003@edu.xamk.fi>
2023-09-13 12:13:55 +0300

    notes.py argument passing

--- notes.py
+++ notes.py
@@ -5,6 +5,18 @@ import sys
 
 # main function
 def main() -> int:
+    # Arguments by sys.argv 
+    print(sys.argv)
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[1] + sys.argv[2])
+    print(23 + 59)
+    a = int(sys.argv[1])
+    b = int(sys.argv[2])
+    print(a + b)
+
+    # Return value
     return(0)
 
 # main function definition
