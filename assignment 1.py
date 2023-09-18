2def7a8bf1c653a9a45c2c4600916b507ee12d82
Aleksandra Balashova <ealba003@edu.xamk.fi>
2023-09-18 10:40:15 +0300

    assignment.py: Added note view (3 print lines)

--- assignment 1.py
+++ assignment 1.py
@@ -1,2 +1,8 @@
 username = input("Username: ")
 password = input("Password: ")
+
+print("New user " + username + "registered")
+
+print("1.Create a note")
+print("2.Retrieve and read new notes")
+print("3.Delete note")
