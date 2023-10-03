6e09c4db65d82e716f880c3370ea17e5228f9c67
Aleksandra Balashova <ealba003@edu.xamk.fi>
2023-10-03 12:52:10 +0300

    task1.py: 'choose' option added to the main app loop

--- task1.py
+++ task1.py
@@ -16,3 +16,23 @@ def delete_note(user):
 def main():
     user = ""
     while True:
+        if user == "":
+            pass
+        else:
+            print("Options:")
+            print("1. Create a new note")
+            print("2. Retrieve and read notes")
+            print("3. Delete a note")
+            print("4. Logout")
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                create_note(user)
+            elif choice == "2":
+                retrieve_notes(user)
+            elif choice == "3":
+                delete_note(user)
+            elif choice == "4":
+                user = ""
+            else:
+                print("Invalid choice. Please try again.")
